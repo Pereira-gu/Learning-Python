ea53da29ce3e738a370a6b47d29191b3760a04f4
Gustavo Dos Santos Pereira <pereira.worker@gmail.com>
2025-10-02 23:11:19 -0300

    adicionei função de adicionar comentarios

--- MyBlog/posts/views.py
+++ MyBlog/posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Post 
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import CommentForm 
 
 def post_list(request):
     posts_list = Post.objects.all().order_by('-created_at') 
@@ -7,6 +8,20 @@ def post_list(request):
     return render(request, 'posts/post_list.html', context)
 
 def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
-    context = {'post':post}
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all().order_by('-created_at')
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    context = {
+        'post':post,
+        'comments': comments,
+        'form': form,
+        }
     return render(request, 'posts/post_detail.html', context)
